d39e6b65f54bc9d2a66ee8d4a4f71c674eb2e64c
Farshid-997 <arnob.farshid@gmail.com>
2022-04-17 14:27:12 +0600

    commited

---
+++ src/Component/CourseDescription/CourseDescription.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CourseDescription = () => {
+    return (
+        <div>
+            <h1>This is  our Course Description</h1>
+        </div>
+    );
+};
+
+export default CourseDescription;
---
+++ src/Component/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CourseDescription from '../CourseDescription/CourseDescription';
+
+const Home = () => {
+    return (
+        <div>
+            <CourseDescription></CourseDescription>
+        </div>
+    );
+};
+
+export default Home;
